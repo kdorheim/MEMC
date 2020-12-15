1a5c852c9e08226d03f8edcc826497fc6bea2541
kdorheim <kalyn.dorheim@pnnl.gov>
2020-12-14 20:59:42 -0500

    add MEND 2013 tests and functions

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' The parameter values for MEND 2013
+#'
+#' @format A data table consisting of 4 columns with inputs from \href{doi.org/10.1890/12-0681.1}{Wang et al. 2013}.
+#' \describe{
+#' \item{parameter}{String character of the default MEND parameters.}
+#' \item{description}{String character describing the parameter.}
+#' \item{units}{String character of the parameter units.}
+#' \item{value}{Numeric values taken from the table 2 of Wang et al. 2013}
+#' }
+#' @family parameters
+#' @references \href{https://doi.org/10.1890/12-0681.1}{Wang et al. 2013}
+"MEND2013_params"
